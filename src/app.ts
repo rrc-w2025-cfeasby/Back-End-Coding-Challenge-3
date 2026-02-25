6babe6e919f60f93b611c4911bf32927b23e2e20
Chris Feasby <cfeasby@academic.rrc.ca>
2026-02-24 18:41:25 -0600

    Update app for health check, products and add service account to girignore

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
 import express, { Express } from "express";
+import productRoutes from "./api/v1/routes/productRoutes";
 
 const app: Express = express();
+app.use(express.json());
+
+app.use("/api/v1/products", productRoutes);
+
+// Health Route
+app.get("/api/v1/health", (req, res) => {
+  res.json({
+    status: "OK",
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+    version: "1.0.0"
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
